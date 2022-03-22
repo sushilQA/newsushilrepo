f819030109720d1862def3214abbc23536db056e
user <user@192.168.1.37>
2022-03-22 09:23:42 +0530

    new class added

---
+++ SeleniumProject/src/test/java/SeleniumProject/SeleniumProject/MainClass2.java
@@ -0,0 +1,24 @@
+package SeleniumProject.SeleniumProject;
+
+import javax.swing.plaf.synth.SynthSpinnerUI;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import com.thoughtworks.selenium.Wait;
+
+public class MainClass2 {
+	
+	@Test
+	public void test1() throws InterruptedException  {	
+		
+		System.out.println("MAIN CLASS 2 SUSHIL");
+	
+	}
+
+}
